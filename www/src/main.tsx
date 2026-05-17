80aca3900ec6d689d9332dbbeec02aa5f9d77cb3
liam <liam@scalzulli.com>
2026-05-17 19:01:43 -0400

    Add toast confirmation when copying node text (#152)

--- www/src/main.tsx
+++ www/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './app.tsx';
+import { Toaster } from './components/ui/sonner.tsx';
 import './index.css';
 import { EditorSettingsProvider } from './providers/editor-settings-provider.tsx';
 import { TreeSitterProvider } from './providers/tree-sitter-provider.tsx';
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <TreeSitterProvider>
       <EditorSettingsProvider>
+        <Toaster richColors />
         <App />
       </EditorSettingsProvider>
     </TreeSitterProvider>

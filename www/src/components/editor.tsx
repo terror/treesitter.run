c3a278fa2aa9ecfd69bd7169698146aa2e6d7be3
liam <liam@scalzulli.com>
2026-05-19 22:28:09 -0400

    Remove codemirror language extensions (#175)

--- www/src/components/editor.tsx
+++ www/src/components/editor.tsx
@@ -44,7 +44,7 @@ export const Editor = ({
           bracketMatching: true,
           history: true,
           indentOnInput: true,
-          syntaxHighlighting: true,
+          syntaxHighlighting: false,
           foldGutter: false,
           closeBrackets: false,
           autocompletion: false,

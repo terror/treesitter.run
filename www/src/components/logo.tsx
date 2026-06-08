cd157987c8942b8e6658bb9c708f571d91c5ea2e
liam <liam@scalzulli.com>
2026-06-08 11:40:43 -0400

    Align tree logo with readme header (#302)

--- www/src/components/logo.tsx
+++ www/src/components/logo.tsx
@@ -9,8 +9,8 @@ export const Logo = ({ className }: { className?: string }) => (
   >
     <path
       className='fill-emerald-600 dark:fill-emerald-400'
-      d='M12 2.5 7.2 10.15h2.7L5.65 16.8h12.7l-4.25-6.65h2.7Z'
+      d='M12 1.5 6.55 10.35H9.6l-4.85 7.4h14.5l-4.85-7.4h3.05Z'
     />
-    <path className='fill-current' d='M10.55 16.8h2.9v4.2h-2.9Z' />
+    <path className='fill-current' d='M10.4 17.75h3.2V23h-3.2Z' />
   </svg>
 );

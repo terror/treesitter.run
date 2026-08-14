ef1c60495e5a26aab2b0ff0daa69f0e8bbb5e44a
terror <terror@users.noreply.github.com>
2026-08-14 18:09:49 +0000

    Deploy preview for PR 371 🛫

---
+++ pr-preview/pr-371/tree-sitter-gstlaunch.highlights.scm
@@ -0,0 +1,19 @@
+(identifier) @variable
+(string_literal) @string
+(string_content) @string
+(property
+  (identifier) @property)
+(simple_element
+  type: (identifier) @type)
+
+[
+  "!"
+  "="
+  "/"
+  "."
+] @operator
+
+[
+  "("
+  ")"
+] @punctuation.bracket

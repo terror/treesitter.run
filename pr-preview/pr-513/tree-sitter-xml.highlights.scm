3637c8a9c25712a77cf8ed7c0a4e65f738baf542
terror <terror@users.noreply.github.com>
2026-09-05 05:06:58 +0000

    Deploy preview for PR 513 🛫

---
+++ pr-preview/pr-513/tree-sitter-xml.highlights.scm
@@ -0,0 +1,22 @@
+(Comment) @comment
+(CharData) @string
+(CData) @string
+(Name) @tag
+(VersionNum) @number
+(EncName) @string.special
+(AttValue) @string
+(Attribute
+  (Name) @attribute)
+
+[
+  "<?"
+  "?>"
+  "<"
+  "</"
+  ">"
+  "/>"
+  "="
+  "&"
+  "&#"
+  "&#x"
+] @operator

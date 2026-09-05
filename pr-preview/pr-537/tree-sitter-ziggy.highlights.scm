365b59b5cfd0ebe2a6f74e51898a33c6b43a1192
terror <terror@users.noreply.github.com>
2026-09-05 22:34:31 +0000

    Deploy preview for PR 537 🛫

---
+++ pr-preview/pr-537/tree-sitter-ziggy.highlights.scm
@@ -0,0 +1,36 @@
+[
+  (true)
+  (false)
+] @constant.builtin.boolean
+
+(null) @constant.builtin
+(number) @constant.numeric
+
+(struct_field
+  key: (_) @property)
+
+(enum) @constant
+
+[
+  (string)
+  (line_string)*
+] @string
+
+(comment) @comment.line
+
+(escape_sequence) @constant.character.escape
+
+(ERROR) @error
+
+"," @punctuation.delimiter
+
+[
+  "["
+  "]"
+  "{"
+  "}"
+  "("
+  ")"
+] @punctuation.bracket
+
+[ "=" ":" ] @punctuation
